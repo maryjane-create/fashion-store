cb2c3b7c6aed3f22036696d5224f3b0b0f896af9
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-10-22 00:32:41 +0100

    added files

--- main.go
+++ main.go
@@ -14,12 +14,31 @@ func init() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	templ.ExecuteTemplate(w, "index.html", nil)
-
 }
 
+func about(w http.ResponseWriter, r *http.Request) {
+	templ.ExecuteTemplate(w, "about.html", nil)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	templ.ExecuteTemplate(w, "sign-up.html", nil)
+}
+
+func help(w http.ResponseWriter, r *http.Request) {
+	templ.ExecuteTemplate(w, "help.html", nil)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	templ.ExecuteTemplate(w, "contact.html", nil)
+}
 func main() {
 	logo := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets", logo))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/help", help)
+	http.HandleFunc("/contact", contact)
+
 	http.ListenAndServe(":9999", nil)
 }
